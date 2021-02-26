09fde390c04d394b09f1db08e3db88b4346c6375
熊谷英明 <bleeding.heart.699@gmail.com>
2021-02-26 21:22:15 +0900

    aggregate_failures の追記

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,35 +42,43 @@ RSpec.describe User, type: :model do
 
   context "name を指定していないとき" do
     it "ユーザー作成に失敗する" do
-      user = User.new(name: nil, email: "foo@example.com", password: "password")
-      expect(user).to be_invalid
-      expect(user.errors[:name]).to include("can't be blank")
+      aggregate_failures do
+        user = User.new(name: nil, email: "foo@example.com", password: "password")
+        expect(user).to be_invalid
+        expect(user.errors[:name]).to include("can't be blank")
+      end
     end
   end
 
   context "nameの文字数が３文字以下の場合" do
     it "ユーザー作成に失敗する" do
-      user = User.new(name: "ann", email: "ann@example.com", password: "password")
-      expect(user).to be_invalid
-      # binding.pry
-      expect(user.errors[:name]).to include("is too short (minimum is 4 characters)")
+      aggregate_failures do
+        user = User.new(name: "ann", email: "ann@example.com", password: "password")
+        expect(user).to be_invalid
+        # binding.pry
+        expect(user.errors[:name]).to include("is too short (minimum is 4 characters)")
+      end
     end
   end
 
   context "nameの文字数11文字以上の場合" do
     it "ユーザー作成に失敗する" do
-      user = User.new(name: "xxxxxxxxxxx", email: "xxxxxxxxxxx@example.com", password: "password")
-      expect(user).to be_invalid
-      expect(user.errors[:name]).to include("is too long (maximum is 10 characters)")
+      aggregate_failures do
+        user = User.new(name: "xxxxxxxxxxx", email: "xxxxxxxxxxx@example.com", password: "password")
+        expect(user).to be_invalid
+        expect(user.errors[:name]).to include("is too long (maximum is 10 characters)")
+      end
     end
   end
 
   context "すでに同じname が存在しているとき" do
     it "ユーザー作成に失敗する" do
-      User.create!(name: "taro", email: "taro@example.com", password: "password")
-      user = User.new(name: "taro", email: "yyy@example.com", password: "password")
-      expect(user).to be_invalid
-      expect(user.errors[:name]).to include("has already been taken")
+      aggregate_failures do
+        User.create!(name: "taro", email: "taro@example.com", password: "password")
+        user = User.new(name: "taro", email: "yyy@example.com", password: "password")
+        expect(user).to be_invalid
+        expect(user.errors[:name]).to include("has already been taken")
+      end
     end
   end
 end
