0fc2b5793cafac3db0300c36afa52099962fb83b
熊谷英明 <bleeding.heart.699@gmail.com>
2021-03-04 19:54:49 +0900

    routes.rbのエラーを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-
-  namespace :api, format: 'json' do
+  namespace :api, format: "json" do
     namespace :v1 do
       resources :article
-      end
     end
   end
-  resources :articles
-  root to: 'people#index'
-  
+
+  resources :article
+  root to: "article#index"
+
   mount_devise_token_auth_for "User", at: "auth"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
