bb51f86ae13231636acb816b85c4120b8cffb3a5
熊谷英明 <bleeding.heart.699@gmail.com>
2021-03-05 12:54:13 +0900

    routes.rbの

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  namespace :api, format: "json" do
+  namespace :api do
     namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth"
       resources :article
     end
   end
 
-  resources :article
   root to: "article#index"
 
-  mount_devise_token_auth_for "User", at: "auth"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
