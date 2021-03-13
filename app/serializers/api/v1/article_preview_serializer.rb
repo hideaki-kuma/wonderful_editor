21727fe8ee720b43a31fdac78ba9329a14516a65
熊谷英明 <bleeding.heart.699@gmail.com>
2021-03-13 21:42:55 +0900

    rubocopの設定を修正

--- app/serializers/api/v1/article_preview_serializer.rb
+++ app/serializers/api/v1/article_preview_serializer.rb
@@ -1,4 +1,7 @@
 class Api::V1::ArticlePreviewSerializer < ActiveModel::Serializer
   attributes :id, :title, :updated_at
+
+  belongs_to :user, serializer: Api::V1::UserSerializer
+  # ↑模範を参考に追加
 end
 # 記事一覧で返してほしいjsonの値
